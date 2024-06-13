4af511e2660d515ca1f6f953df09da4580ec9779
MG16 <dere_de@gmx.at>
2024-06-13 14:18:10 +0200

    Integrating examples

--- main.py
+++ main.py
@@ -4,45 +4,32 @@ from pathlib import Path
 import random
 
 
-""" 
-random.seed(4)
-    maze = get_maze_text()
-    # maze = [[1, 1, 0], [0, 0.5, 1], [1, 1, 0]]
-    width = 4
-    length = 6
-    maze_generator = DFSMaze(width, length)
-    maze_list = maze_generator.create_maze()
-    maze = Maze(maze_list)
-
- """
-
-""" 
-Config long maze:
-random.seed(15)
-    width = 20
-    length = 40
-
-    width = 30
-    length = 60
-
- """
-
-
-def main():
-    """random.seed(15)
-    width = 40
-    length = 60
-    # maze = get_maze_text()
-    # maze = [[1, 1, 0], [0, 0.5, 1], [1, 1, 0]]
-
-    maze_generator = DFSMaze(width, length)
-    maze_generator.create_maze()
-    maze_list = maze_generator.maze"""
+def read_maze(filename: str = "maze_examples/maze_60_40.txt"):
     maze = Maze.import_maze("maze_examples/maze_60_40.txt")
     solver = MazeSolver(maze, dfs_algorithm)
     path = solver.solve_maze()
-    maze.matplotlib_view(path=path, pausing=0.001, marker_size=30)
-    # maze_generator.export_maze("maze_examples/maze_60_40.txt")
+    maze.matplotlib_view(path=path, pausing=0.0005, marker_size=20)
+
+
+def maze_example(width: int = 10, length: int = 20):
+    maze_generator = DFSMaze(width, length)
+    maze_generator.create_maze()
+    maze_list = maze_generator.maze
+    maze = Maze(maze_list)
+    solver = MazeSolver(maze, dfs_algorithm)
+    path = solver.solve_maze()
+    maze.matplotlib_view(path=path, pausing=0.001, marker_size=40)
+
+
+def main():
+    ## read_maze via files
+
+    # read_maze()
+
+    ## generate own maze with provided length and width (e.g. 20 and 10)
+
+    # maze_example()
+    pass
 
 
 if __name__ == "__main__":
